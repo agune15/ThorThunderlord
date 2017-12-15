03a4ae1a4e0e69ecfe594b1640db81995369e446
Jon Cervellera Dominguez <joon_1012@hotmail.com>
2017-12-15 12:23:44 +0100

    Update Scene Manager (by Melusas)
    Implement Level logic and the video of the background

---
+++ Assets/Menu/Sripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    LevelLogic levelLoader;
+    public int goToScene;
+
+    private void Start()
+    {
+        levelLoader = GameObject.Find("Managers").GetComponent<LevelLogic>();
+    }
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        levelLoader.StartLoad(goToScene);
+
+    }
+
+}
