c5e7843767f0ca23b303ee53627fba9ce0cb695e
JonCervellera <32324044+JonCervellera@users.noreply.github.com>
2017-12-16 20:28:48 +0100

    Conditions of Victory and Defeat.
    I put conditions when you die, scen of game over, and conditions when you enter on a trigger, victory codition.

---
+++ Assets/Scripts/Scenes/EndLevel.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    private LevelLogic managerScenes;
+    public GameObject sceneLevelManager;
+
+    private void Start()
+    {
+        sceneLevelManager = GameObject.FindWithTag("GameManager");
+        managerScenes = sceneLevelManager.GetComponent<LevelLogic>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Thor")
+        {
+            managerScenes.LoadEndScene();
+        }
+    }
+}
