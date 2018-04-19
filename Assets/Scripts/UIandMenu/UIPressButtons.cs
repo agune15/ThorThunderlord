c2d9b9ba61f1aefca9fc5804a8c3b75a72c6175d
Alex Nebot Oller <alex.nebot.sds@gmail.com>
2018-04-19 23:41:49 +0200

    completed main menu fix
    completed main menu fix

--- Assets/Scripts/UIandMenu/UIPressButtons.cs
+++ Assets/Scripts/UIandMenu/UIPressButtons.cs
@@ -6,7 +6,18 @@ using UnityEngine.UI;
 public class UIPressButtons : MonoBehaviour {
 
     public Button startButton;
-    public Button exitButton;
+    public Button mainExitButton;
+    public Button optionsExitButton;
+    public Button videoExitButton;
+    public Button soundExitButton;
+    public Button controlsExitButton;
+    public Button languageExitButton;
+
+    public Animator optionsPanelAnimator;
+    public Animator videoPanelAnimator;
+    public Animator soundPanelAnimator;
+    public Animator controlsPanelAnimator;
+    public Animator languagePanelAnimator;
 
     [SerializeField] CameraTransition cameraTransition;
 
@@ -18,11 +29,16 @@ public class UIPressButtons : MonoBehaviour {
     void Update () {
         if(Input.GetKeyDown(KeyCode.E))
         {
-            if(startButton.isActiveAndEnabled && !cameraTransition.IsEasing() && startButton.interactable) startButton.onClick.Invoke();
+            if (startButton.isActiveAndEnabled && !cameraTransition.IsEasing() && startButton.interactable) startButton.onClick.Invoke();
         }
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            if(exitButton.isActiveAndEnabled && !cameraTransition.IsEasing() && exitButton.interactable) exitButton.onClick.Invoke();
+            if (mainExitButton.isActiveAndEnabled && !cameraTransition.IsEasing() && mainExitButton.interactable) mainExitButton.onClick.Invoke();
+            else if (optionsPanelAnimator.GetCurrentAnimatorStateInfo(0).IsName("OpenOptionsPanels") && optionsExitButton.interactable) optionsExitButton.onClick.Invoke();
+            else if (videoPanelAnimator.GetCurrentAnimatorStateInfo(0).IsName("OpenVideoPanels") && videoExitButton.interactable) videoExitButton.onClick.Invoke();
+            else if (soundPanelAnimator.GetCurrentAnimatorStateInfo(0).IsName("OpenSoundOptions") && soundExitButton.interactable) soundExitButton.onClick.Invoke();
+            else if (controlsPanelAnimator.GetCurrentAnimatorStateInfo(0).IsName("openControlsOption") && controlsExitButton.interactable) controlsExitButton.onClick.Invoke();
+            else if (languagePanelAnimator.GetCurrentAnimatorStateInfo(0).IsName("OpenLanguageOptions") && languageExitButton.interactable) languageExitButton.onClick.Invoke();
         }
     }
 }
