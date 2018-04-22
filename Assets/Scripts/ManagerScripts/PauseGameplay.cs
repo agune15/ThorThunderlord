626130dc3040c547574f1be932a4135aa3c8245a
Alex Nebot Oller <alex.nebot.sds@gmail.com>
2018-04-23 00:16:27 +0200

    pause menu fixes (wib)
    pause menu fixes (wib)

--- Assets/Scripts/ManagerScripts/PauseGameplay.cs
+++ Assets/Scripts/ManagerScripts/PauseGameplay.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseGameplay : MonoBehaviour {
 
-    public bool isGamePaused = false;
+    public GameObject canvas;
+    Image pauseBackground;
+
+    [HideInInspector] public bool isGamePaused = false;
     float pauseTimeScale = 0;
     float runningTimeScale = 1;
 
-    public GameObject canvas;
+    public float pauseBackgroundMaxAlpha;
+    float pauseBackgroundFadeTime;
+    float pauseBackgroundFadeTimer;
 
+    private void Start()
+    {
+        pauseBackground = GameObject.Find("PauseBackground").GetComponent<Image>();
+    }
+    
     public void Pause()
     {
         isGamePaused = !isGamePaused;
@@ -20,12 +31,26 @@ public class PauseGameplay : MonoBehaviour {
             {
                 canvas.SetActive(true);
                 Time.timeScale = pauseTimeScale;
+                StartCoroutine(PauseBackground());
             }
         }
         else
         {
+            pauseBackground.color = new Color(pauseBackground.color.r, pauseBackground.color.g, pauseBackground.color.b, 0);
             canvas.SetActive(false);
             Time.timeScale = runningTimeScale;
         }
+    }
+
+    IEnumerator PauseBackground ()
+    {
+        pauseBackground.color = new Color(pauseBackground.color.r, pauseBackground.color.g, pauseBackground.color.b, 0);
+
+        while (pauseBackgroundFadeTimer <= pauseBackgroundFadeTime)
+        {
+            //
+        }
+
+        yield return 0;
     }
 }
