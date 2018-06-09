d0dfff95b0d9a0208b1522fbeaa85a8cc41967cb
Alex Nebot Oller <alex.nebot.sds@gmail.com>
2018-06-09 02:52:39 +0200

    light fixes, light behaviour implementations
    light fixes, light behaviour implementations

---
+++ Assets/FireLightBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireLightBehaviour : MonoBehaviour {
+
+    Animator fireLightAnimator;
+
+
+	void Start () {
+        fireLightAnimator = GetComponent<Animator>();
+        int intensityVariant = Random.Range(1, 4);
+
+        fireLightAnimator.SetInteger("desiredIntensity", intensityVariant);
+	}
+}
