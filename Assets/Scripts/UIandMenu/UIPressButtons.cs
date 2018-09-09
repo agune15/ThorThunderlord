ca68450aa51a7564ae780ca4030220f71b46adc9
Alex Nebot Oller <alex.nebot.sds@gmail.com>
2018-09-09 23:48:10 +0200

    changed StartButton sprite, changed StartButton interaction input
    changed StartButton sprite, changed StartButton interaction input

--- Assets/Scripts/UIandMenu/UIPressButtons.cs
+++ Assets/Scripts/UIandMenu/UIPressButtons.cs
@@ -31,7 +31,7 @@ public class UIPressButtons : MonoBehaviour {
     }
     
     void Update () {
-        if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        if(Input.anyKeyDown)
         {
             if (startButton.isActiveAndEnabled && !cameraTransition.IsEasing() && startButton.interactable) startButton.onClick.Invoke();
         }
